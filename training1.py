54f1644f0d48a46458248550b5fc12407d7c5fcb
Markiyan  Samiylenko <135683234+markotutbuv@users.noreply.github.com>
2023-07-04 14:25:47 -0700

    adding more form lesson

--- training1.py
+++ training1.py
@@ -23,3 +23,47 @@ fruit = "apple"
 """
 really long comment that helps understand code better
 """
+
+#Right way of naming variables
+name = "John"
+age = 25
+is_student = True
+
+#Right way of using constants
+PI = 3.14159
+Max_Value = 100
+DEFAULT_NAME = "John Doe"
+
+#example of right way of using combining variables
+width = 10
+height = 5
+area = width * height
+
+"""
+exampels of how not to do it 
+not explaining name of variable
+n = "John"
+x = True
+f = 10.5
+using numbers or special symbols in front of variable 
+2variable = "invalid"
+$price = 100
+
+using reserved word for variable
+class = "invalid"
+if = True
+
+List of reserved words 
+False, None, True, and, as, assert, async, await, break, class, continue, def, 
+del, elif, else, except, finally, for, from, global, if, import, in, is, 
+lambda, nonlocal, not, or, pass, raise, return, try, while, with, yield
+
+"""
+
+# Showing couple variables at the same time
+
+x = y = z = 99
+a, b, c, = 1, 2, 3
+
+print (x, y, z, end="\t")
+print (a, b, c, sep=":")
