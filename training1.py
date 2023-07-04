21f5564909cef6b6824d6663a375c2146b92766d
Markiyan  Samiylenko <135683234+markotutbuv@users.noreply.github.com>
2023-07-04 13:38:41 -0700

    Update training1.py
    added code samples of comment and variables

--- training1.py
+++ training1.py
@@ -1,1 +1,25 @@
 print("Hello World")
+# copying examples from Lesson#1
+
+# showig how variables are shown in the program
+name = "John"
+age = 25
+height = 1.67
+is_student = True
+has_high_degree = None
+
+print(name)
+print(age)
+print(height)
+print(is_student)
+print(has_high_degree)
+
+
+# showing comments
+    #two types of coments - short and long
+
+fruit = "apple"
+
+"""
+really long comment that helps understand code better
+"""
